491871f95ff48189965929b67ade42b5810be2d8
Barbora Rihova <barborka.rihova@gmail.com>
2022-04-11 12:55:04 +0200

    linear_search, pattern_search

--- searching.py
+++ searching.py
@@ -16,16 +16,49 @@ def read_data(file_name, field):
     with open(file_path, mode = "r") as json_file:
         data = json.load(json_file)
     keys = data.keys()
-    print(keys)
     if field not in keys:
-        return "NaN"
+        return "None"
 
     return data[field]
+def linear_search(sekvence, hledane_cislo):
+    cisla = read_data("sequential.json", sekvence)
+    positions = []
+    count = 0
+    i = 0
+    for cislo in cisla:
+        if cislo == hledane_cislo:
+            count = count + 1
+            positions.append(i)
+        i = i + 1
+    print(count)
+    print(positions)
+    vysledky = dict(zip(["count", "positions"], [count, positions]))
+    return vysledky
+
+    # output = {"positions" : positions, "count": count}
+def pattern_search(sekvence, hledana_sekvence):
+    i = 0
+    delka_vzoru = len(hledana_sekvence)
+    if len(sekvence) < delka_vzoru:
+        return "Nelze"
+    seznam_indexu = []
+    while i < len(sekvence) - (delka_vzoru-1):
+        sekvence_v_seznamu = sekvence[i:i+(delka_vzoru)]
+        if sekvence_v_seznamu == hledana_sekvence:
+            seznam_indexu.append(i)
+
+        i = i + 1
+    mnozina_indexu = set(seznam_indexu)
+    return mnozina_indexu
+
+
+
 
 
 def main():
-    unordered_numbers = read_data("sequential.json", "unordered_numbers")
-    print(unordered_numbers)
+    sekvence = read_data("sequential.json", "dna_sequence")
+    print(linear_search("unordered_numbers", 54))
+    print(pattern_search(sekvence, "AT"))
 
 
 if __name__ == '__main__':
